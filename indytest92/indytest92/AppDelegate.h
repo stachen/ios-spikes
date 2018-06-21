88c0bec80441dfdda7b73e2f241780c5da35c2af
Stanley Chen <stanleychen24w@gmail.com>
2018-06-21 02:58:30 -0700

    Initial checkin

---
+++ indytest92/indytest92/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  indytest92
+//
+//  Created by Stanley Chen on 6/16/18.
+//  Copyright © 2018 Stanley Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ indytest92/indytest92/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  indytest92
+//
+//  Created by Stanley Chen on 6/16/18.
+//  Copyright © 2018 Stanley Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
