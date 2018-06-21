0c9c36001abb310f78f6ff3b9452f2ab71ec436d
Stanley Chen <stanleychen24w@gmail.com>
2018-06-21 11:32:31 -0700

    MyFirstApplication initial project creation

---
+++ MyFristApplication/MyFristApplication/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyFristApplication
+//
+//  Created by Stanley Chen on 6/21/18.
+//  Copyright © 2018 Stanley Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
